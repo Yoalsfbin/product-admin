ae724e834503d6edda879063b0d2c30a77c6302c
Yoalsfbin <y.yokoyfb@gmail.com>
2025-08-10 18:31:31 +0900

    [feat]usersテーブルのマイグレーション更新

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
